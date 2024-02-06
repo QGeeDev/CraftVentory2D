0bd97ba53d9dd6d6abefd179435a5b672b7651fe
QG <qg@qgdev.co.uk>
2024-02-06 23:22:21 +0000

    Added quantity check to inventory slot

--- data/inventory/inventory_slot.gd
+++ data/inventory/inventory_slot.gd
@@ -4,4 +4,11 @@ extends Resource
 const MAX_STACK_SIZE: int = 64;
 
 @export var item_data: ItemData;
-@export_range(1, MAX_STACK_SIZE) var quantity: int = 1;
+@export_range(1, MAX_STACK_SIZE) var quantity: int = 1 : set = set_quantity;
+
+func set_quantity(val: int) -> void:
+	quantity = val;
+	
+	if quantity > 1 and not item_data.stackable:
+		quantity = 1;
+		push_warning("%s not stackable, setting quantity to 1" % item_data.name);
