a304f88d2d8786600946e8d20b064707503198b2
QG <qg@qgdev.co.uk>
2024-02-11 12:49:40 +0000

    Inventory is interactable through interface

--- data/inventory/inventory_data.gd
+++ data/inventory/inventory_data.gd
@@ -1,7 +1,9 @@
 class_name InventoryData
 extends Resource
 
+signal inventory_interact(inventory_data: InventoryData, index: int, button: int)
+
 @export var inventory_slots: Array[InventorySlot] = [];
 
-func on_slot_clicked(index: int, button: int):
-	print_debug("Inventory interact")
+func on_slot_clicked(index: int, button: int) -> void:
+	inventory_interact.emit(self, index, button)
--- examples/InventoryInterface.gd
+++ examples/InventoryInterface.gd
@@ -4,4 +4,8 @@ extends Control
 
 
 func set_inventory_data(inventory_data: InventoryData) -> void:
+	inventory_data.inventory_interact.connect(_on_inventory_interact)
 	inventory.set_inventory_data(inventory_data)
+	
+func _on_inventory_interact(inventory_data: InventoryData, index: int, button: int) -> void:
+	print_debug("%s %s %s" % [inventory_data, index, button])
