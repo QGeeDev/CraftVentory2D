ca956f5a7ebeb5461c8e2c04b146aa8cd99a4e1a
QG <qg@qgdev.co.uk>
2024-03-19 21:05:20 +0000

    Added passthrough for click, having inv open no longer stops click on area2d

--- examples/LootChest.gd
+++ examples/LootChest.gd
@@ -8,4 +8,5 @@ func _on_area_2d_input_event(viewport, event, shape_idx) -> void:
 	if event is InputEventMouseButton \
 	and event.button_index == MOUSE_BUTTON_LEFT \
 	and event.is_pressed():
+		print("emitting")
 		toggle_inventory_visible.emit(self)
