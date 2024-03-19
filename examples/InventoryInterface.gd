fe99d68a2b1b9cdae9e0ebf0ad47f04a6203ae40
QG <qg@qgdev.co.uk>
2024-03-19 21:42:37 +0000

    Added external inventory

--- examples/InventoryInterface.gd
+++ examples/InventoryInterface.gd
@@ -2,8 +2,10 @@ extends Control
 
 @onready var inventory = $Inventory
 @onready var held_slot = $SelectedSlot
+@onready var external_inventory = $ExternalInventory
 
 var held_slot_data: InventorySlot
+var external_inventory_owner
 
 func _physics_process(delta: float) -> void:
 	if(held_slot.visible):
@@ -33,5 +35,9 @@ func update_selected_slot() -> void:
 	else:
 		held_slot.hide()
 		
-func set_external_inventory(external_inventory_owner) -> void:
-	print(external_inventory_owner)
+func set_external_inventory(_external_inventory_owner) -> void:
+	external_inventory_owner = _external_inventory_owner
+	var inventory_data = external_inventory_owner.inventory_data
+	inventory_data.inventory_interact.connect(_on_inventory_interact)
+	external_inventory.set_inventory_data(inventory_data)
+	external_inventory.show()
