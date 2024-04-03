70658c9087e2e53568dfd35a05c99ec2dac91af9
QG <qg@qgdev.co.uk>
2024-04-03 21:44:55 +0100

    Added initial crafting scene

--- data/crafting/crafting_engine.gd
+++ data/crafting/crafting_engine.gd
@@ -6,27 +6,27 @@ const ANY_IDENTIFIER = "*"
 var _recipes: Array[CraftingRecipe] = []
 
 func add_recipe(recipe: CraftingRecipe) -> void:
-    recipes.Append(recipe)
+	_recipes.append(recipe)
 
 func is_valid_recipe(components: Array[AbstractItem]) -> bool:
-    for var recipe in recipes:
-        if check_recipe_match(components):
-            return true
-    return false
+	for recipe in _recipes:
+		if check_recipe_match(components, recipe):
+			return true
+	return false
 
 func check_recipe_match(inputComponents: Array[AbstractItem], recipe: CraftingRecipe) -> bool:
-    var recipeComponents = recipe.components
-    if recipeComponents.size() != inputComponents.size():
-        return false
+	var recipeComponents = recipe.components
+	if recipeComponents.size() != inputComponents.size():
+		return false
 
-    for i in components.size():
-        recipeItem = recipeComponents[i]
-        inputItem = inputComponents[i]
+	for i in recipeComponents.size():
+		var recipeItem = recipeComponents[i]
+		var inputItem = inputComponents[i]
 
-        if(recipeItem.category != inputItem.category):
-            return false
+		if(recipeItem.category != inputItem.category):
+			return false
 
-        if(recipeItem.id != inputItem.id && recipeItem.id != ANY_IDENTIFIER):
-            return false
+		if(recipeItem.id != inputItem.id && recipeItem.id != ANY_IDENTIFIER):
+			return false
 
-    return true
+	return true
