ba83ef20fdd3936c7528ecf0b44899735ba91403
QG <qg@qgdev.co.uk>
2024-04-08 22:07:02 +0100

    Fixed atlas textures again

---
+++ examples/Crafting/ExampleCraftingScene.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var inventory_interface: Control = $UI/InventoryInterface
+@onready var player_data = $PlayerData
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
