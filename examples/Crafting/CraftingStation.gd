33370b3ab472a570ac05d2ba89954299bb6f03dd
QG <qg@qgdev.co.uk>
2024-04-08 22:25:08 +0100

    Added logs to inventory, added inventory data to crafting example

--- examples/Crafting/CraftingStation.gd
+++ examples/Crafting/CraftingStation.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
 @export var engine: CraftingEngine
+@export var componentsSlots: Array[InventorySlot]
+@export var craftingResult: InventorySlot
 
 signal toggle_crafting_visible(external_inventory_owner)
 
-func _on_area_2d_input_event(viewport, event, shape_idx) -> void:
+func _on_area_2d_input_event(_viewport, event, _shape_idx) -> void:
 	if event is InputEventMouseButton \
 	and event.button_index == MOUSE_BUTTON_LEFT \
 	and event.is_pressed():
-
 		toggle_crafting_visible.emit(self)
--- examples/Crafting/ExampleCraftingScene.gd
+++ examples/Crafting/ExampleCraftingScene.gd
@@ -11,3 +11,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_crafting_station_toggle_crafting_visible(external_inventory_owner):
+	print_debug("Interacted with crafting table")
