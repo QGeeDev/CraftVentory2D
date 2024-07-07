11fff171d517218afeda65ec81f6f5ba97b87c1f
QG <qg@qgdev.co.uk>
2024-07-07 16:13:41 +0100

    WIP

--- scenes/Serialization/RecipeLoader.gd
+++ scenes/Serialization/RecipeLoader.gd
@@ -2,10 +2,16 @@ extends Node
 class_name RecipeLoader
 
 func load_recipe_from_file(filepath: String) -> Array[CraftingRecipe]:
-	var fileContent = _load_string_from_file(filepath)
-	# validate JSON
-	# Parse to recipe list
-	# Return
+	var file_content = _load_string_from_file(filepath)
+	if file_content=="":
+		return []
+		
+	
+	var json_content = _get_data_as_json(file_content)
+	if(!json_content || json_content.size() ==0):
+		return []
+	
+	var recipes = _parse_json_to_recipes(json_content)
 	return []
 	
 func _load_string_from_file(filepath: String) -> String:
@@ -16,3 +22,14 @@ func _load_string_from_file(filepath: String) -> String:
 	var content = file.get_as_text()
 	file.close()
 	return content
+
+func _get_data_as_json(json_string: String) -> Array[Dictionary]:
+	var json = JSON.new()
+	var err = json.parse_string(json_string)
+	if err != OK:
+		printerr("Failed to parse JSON. File Content may not be valid")
+		return []
+	return json.data
+	
+func _parse_json_to_recipes(json_data: Variant):
+	pass
